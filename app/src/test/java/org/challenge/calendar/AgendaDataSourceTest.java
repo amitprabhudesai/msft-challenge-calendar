ada768b4f78150df36f7bc3a101ba2b9f5d0b12c
Amit Prabhudesai <prabhudesai.amit@gmail.com>
2017-07-27 06:33:36 +0530

    Test case stubs for AgendaDataSource

--- app/src/test/java/org/challenge/calendar/AgendaDataSourceTest.java
+++ app/src/test/java/org/challenge/calendar/AgendaDataSourceTest.java
@@ -19,7 +19,68 @@ public class AgendaDataSourceTest {
     }
 
     @Test
+    public void testClear() {
+        agendaDataSource.clear();
+        assertTrue(0 == agendaDataSource.getSectionCount());
+    }
+
+    @Test
+    public void testAddEvent_ExistingSection() {
+        fail("Not implemented yet!");
+    }
+
+    @Test
+    public void testAddEvent_NewSection() {
+        fail("Not implemented yet!");
+    }
+
+    @Test
+    public void testGetSectionCeil() {
+        fail("Not implemented yet!");
+    }
+
+    @Test
+    public void testGetSectionFloor() {
+        fail("Not implemented yet!");
+    }
+
+    @Test
     public void testGetSectionCount() {
         assertTrue(0 == agendaDataSource.getSectionCount());
+    }
+
+    @Test
+    public void testGetEventCount_ValidSectionIndex() {
+        fail("Not implemented yet!");
+    }
+
+    @Test
+    public void testGetEventCount_InvalidSectionIndex() {
+        fail("Not implemented yet!");
+    }
+
+    @Test
+    public void testGetTime_ValidSectionIndex() {
+        fail("Not implemented yet!");
+    }
+
+    @Test
+    public void testGetTime_InvalidSectionIndex() {
+        fail("Not implemented yet!");
+    }
+
+    @Test
+    public void testGetEventItem_ValidSectionIndexAndOffset() {
+        fail("Not implemented yet!");
+    }
+
+    @Test
+    public void testGetEventItem_InvalidSectionIndex() {
+        fail("Not implemented yet!");
+    }
+
+    @Test
+    public void testGetEventItem_InvalidOffsetInSection() {
+        fail("Not implemented yet!");
     }
 }
